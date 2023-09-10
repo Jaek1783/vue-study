2aafa59019e7205b148dbd8f232c7324119eff66
jaekeun Choi <jaek1783@gmail.com>
2023-09-10 20:13:46 +0900

    toRefs 학습

--- src/components/props/ChildrenComponent.vue
+++ src/components/props/ChildrenComponent.vue
@@ -1,8 +1,10 @@
 <template>
+    <h2>props 학습</h2>
     <div>
         {{ name }} 입니다
     </div>
     <button v-on:click="onChange">이름변경</button>
+    <hr/>
 </template>
 
 <script>
---
+++ src/components/torefs/toRefsComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <h2>toRefs, reactive 학습</h2>
+    <div>
+        <span>생선 이름 : {{ name }}</span>
+
+        <span>가격 : {{ price }}</span>
+    </div>
+    <button @click="change">생선 바꾸기</button>
+    <hr/>
+</template>
+
+<script>
+import {reactive, toRefs} from 'vue';
+export default {
+    setup(){
+        const change = ()=>{
+            fish.name = '갈치 ',
+            fish.price = 1000
+        }
+        const fish = reactive({
+            name : '고등어 ',
+            price : 5000
+        });
+
+        return {
+            ...toRefs(fish),
+            change
+        }
+    }
+}
+</script>
